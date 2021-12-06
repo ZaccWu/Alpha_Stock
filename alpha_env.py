3a928c246c1402bafccff2201c85f34e58052647
Ziyue Wu <670122025@qq.com>
2021-12-06 10:58:36 +0800

    Add files via upload

---
+++ alpha_env.py
@@ -0,0 +1,197 @@
+
+import numpy as np
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import seaborn as sns
+from collections import deque
+import random
+import time
+
+import gym
+import os
+import h5py
+
+
+class ALPHA_ENV(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+
+        df = pd.read_csv(r'zztest.csv')
+        self.today = '2018/1/2'
+
+
+        self.stock_all = df['thscode'].unique() # len=454
+        self.test_count = 0  # for testing
+        self.stock_list = df['thscode']
+        self.close = df['CLOSE_AFTER']
+
+        self.action_space = gym.spaces.Box(
+            low=np.array([-1] * 1),
+            high=np.array([1] * 1),
+        )
+        self.observation_space = gym.spaces.Box(
+            low=np.array([-5] * 31),
+            high=np.array([5] * 31)
+        )
+
+        self.seq_time = 48
+        self.profit = 0
+        self.flow = 0
+
+        self.data_train = df.drop(['CLOSE_AFTER'], axis=1)
+        self.close_train = df['CLOSE_AFTER']
+        self.time_stump = df['time']
+
+        # TODO: 临时参数
+        self.K = 10 # 历史期为10
+        self.test_stock_num = 30    # 先用20支股票测试
+
+
+    def reset(self):
+        # if self.util == 'test':
+        #     thscode = self.stock_all[self.test_count]  # 测试时使用指定测试股票
+        #     self.dt = self.data_train[self.stock_list == thscode]
+        #     self.dt1 = np.array(self.dt.iloc[:, 3:])
+        #     self.close1 = self.close_train[self.stock_list == thscode]
+        #     self.time_stump1 = self.time_stump[self.stock_list == thscode]
+        #
+        #     self.test_count+=1
+        #     self.trade_date = 0 # 测试时从头开始跑
+        #else:
+        # 场景的设置
+        self.inventory = 0
+        self.initial_money = 1000000
+        self.total_money = 1000000
+        self.profit = 0
+        self.profit_list = []       # 每个时间点的profit
+        self.portfolio_list = []    # 每个step的资产
+        self.stock_price = 0
+        self.today_buy_port = 0 # 限制每日交易的
+        # 测量指标
+        self.buy_hold = 0
+        self.sp = 0
+        self.maxdrawdown = 0
+        self.mdd = 0
+        self.romad = 0
+
+        #thscode = random.choice(self.stock_all)   # 训练时随机选择股票
+        # self.dt = self.data_train[self.stock_list == thscode]
+        # self.dt1 = np.array(self.dt.iloc[:, 3:])
+        # self.close1 = self.close_train[self.stock_list == thscode]
+        # self.time_stump1 = self.time_stump[self.stock_list == thscode]
+        # self.trade_date = np.random.randint(self.K-1, len(self.close1) - self.seq_time)    # 训练时随机选择时间点开始
+
+
+        Portfolio_unit = 1
+        Rest_unit = 1
+
+        self.holding = [0] * self.test_stock_num
+
+        self.t = 0
+
+        # 所有股票再t时间点往前K步的历史记录
+        all_stock_his_state = []
+
+        self.all_stock_close = []
+
+        for i in range(self.test_stock_num):
+            thscode = self.stock_all[i]
+            self.dt = self.data_train[self.stock_list == thscode]
+            self.feature_at_t = np.array(self.dt.iloc[:, 3:])    # 这支股票的特征
+            self.all_stock_close.append(self.close_train[self.stock_list == thscode])  # 这支股票的收盘价
+            self.time_stump1 = self.time_stump[self.stock_list == thscode]  # 这支股票的时间点
+            self.trade_date = np.random.randint(self.K - 1, len(self.time_stump1) - self.seq_time)
+
+            # k_his_state为
+            k_his_state = self.get_K_his_state(self.trade_date, self.t)
+            all_stock_his_state.append(k_his_state)
+
+
+        self.all_stock_close = np.array(self.all_stock_close).transpose()
+        # 返回列表的维数: (num_stock, window_size_k, feature_dim)
+        return all_stock_his_state
+
+    def get_K_his_state(self, trade_date, t):
+        # 第i支股票在t时间点往前K步的历史记录
+        k_his_state = []  # (window_size_K, feature_dim)
+        for j in range(self.K):
+            his_state = self.feature_at_t[trade_date + t - self.K + j]
+            # add_state = np.array([Portfolio_unit, Rest_unit]).flatten()
+            # his_state = np.hstack([his_state, add_state])
+            k_his_state.append(his_state)
+        return k_his_state
+
+
+    def get_reward(self, profit):
+        reward = 0
+        if 0 < profit <= 0.1:
+            reward = 1
+        if 0.1 < profit <= 0.2:
+            reward = 2
+        if 0.2 <= profit:
+            reward = 4
+        if -0.1 <= profit < 0:
+            reward = -1
+        if -0.2 <= profit < -0.1:
+            reward = -2
+        if profit < -0.2:
+            reward = -4
+        return reward
+
+    def step(self, action):
+        # TODO: 解决action不同的问题
+        # 传入的action：是否买卖股票的向量（按index顺序）\in {0,1,-1}
+
+        # self.all_stock_close[i]是第i支股票所有时段的价格
+        #self.stock_price = self.close1.iloc[self.trade_date + self.t]
+
+        self.stock_price = self.all_stock_close[self.trade_date + self.t] # 所有股票这个时间的价格向量
+
+        # 所有股票的time_stump一样的，就默认用最后一支了
+        today_time = (self.time_stump1.iloc[self.trade_date + self.t]).split(' ')[0]  # 获取日期到天
+
+        # TODO：先复现最简单的情况：无买卖限制
+        self.holding = list(map(lambda x: x[0]+x[1], zip(self.holding, action)))
+        self.cost = sum(list(map(lambda x: x[0]*x[1], zip(self.stock_price, action)))) # +表示支出，-表示收入
+        self.total_money -= self.cost
+
+        self.Portfolio_unit = (self.total_money + sum(list(map(lambda x: x[0]*x[1], zip(self.stock_price, self.holding))))
+                               ) / self.initial_money  # 资产与初始资金比例
+        Rest_unit = self.total_money / self.initial_money                           # 剩余金额占比
+
+        total_profit = (self.total_money + sum(list(map(lambda x: x[0]*x[1], zip(self.stock_price, self.holding))))
+                        ) - self.initial_money
+        # reward = self.get_reward(total_profit / self.initial_money)                 # 传入get_reward的就是收益率
+        self.profit = total_profit / self.initial_money # get profit（收益率）
+
+        self.profit_list.append(self.profit)
+        self.portfolio_list.append(self.Portfolio_unit)
+
+        self.t += 1
+        done = self.seq_time < (self.t + 1)
+
+        sp_std = np.std(self.profit_list)
+        if sp_std<10e-4:
+            sp_std=10e-4
+        self.sp = (np.mean(self.profit_list))/sp_std          # 最后输出全时间段的夏普率（无风险利率3%）
+
+        reward = self.get_reward(self.sp)
+
+        # print(reward, self.holding)
+
+        # 所有股票再t时间点往前K步的历史记录
+        all_stock_his_state = []
+        for i in range(self.test_stock_num):
+            thscode = self.stock_all[i]
+            self.dt = self.data_train[self.stock_list == thscode]
+            self.feature_at_t = np.array(self.dt.iloc[:, 3:])    # 这支股票的特征
+            k_his_state = self.get_K_his_state(self.trade_date, self.t)
+            all_stock_his_state.append(k_his_state)
+
+        state = all_stock_his_state
+
+
+        return state, reward, done, {}
---
+++ alphastock.py
@@ -0,0 +1,350 @@
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import random
+import time
+from collections import deque
+from alpha_env import ALPHA_ENV
+
+# Hyper Parameters for PG Network
+GAMMA = 0.95  # discount factor
+LR = 0.01  # learning rate
+
+# Use GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class StdAttn(nn.Module):
+    def __init__(self, input_size, att_dim, dropout):
+        super().__init__()
+        self.attention_dim = att_dim
+        self.query_transform = nn.Linear(input_size, att_dim)
+        self.key_transform = nn.Linear(input_size, att_dim)
+        self.attn_score = nn.Sequential(
+            nn.Tanh(),
+            nn.Dropout(p=dropout),
+            nn.Linear(att_dim, 1),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, q, k, v):
+        # q, v都是(batch*stock_num, window_size_K, hidden_size)
+        # k是(batch*stock_num, hidden_size)
+        embed_dim = q.shape[-1] # hidden_size
+        bsz = q.shape[0]        # batch*stock_num
+        num_queries = q.shape[-2]   # window_size_K (Q)
+
+        if not k.shape[-1] == embed_dim or not v.shape[-1] == embed_dim:
+            raise ValueError("Assume identical embedding dim for q, k, v")
+
+        q = q.reshape(num_queries, bsz, embed_dim) # q: (window_size_K, batch*stock_num, hidden_size)
+        # v = v.unsqueeze(dim=1)  # v: (batch*stock_num, 1, window_size_K, hidden_size)
+
+        # q = q.unsqueeze(dim=-2)
+
+        # convert to n_dim == 4
+        # if num_queries == 1:
+        #     k = k.unsqueeze(dim=1)
+        #     v = v.unsqueeze(dim=1)
+
+        # [Q * (B*I) * E_lstm] * [E_lstm * att_dim] -> [Q * (B*I) * att_dim]
+        trans_q = self.query_transform(q)
+        # [(B*I) * E_lstm] * [E_lstm * att_dim] -> [(B*I) * att_dim]
+        trans_k = self.key_transform(k)
+
+        # broadcast to (Q, B*I, att_dim)
+        attn_input = trans_k + trans_q
+
+        # transfer to (Q * (B*I), att_dim)
+        attn_input = attn_input.view(-1, attn_input.shape[-1])
+
+        # softmaxed ((Q * (B*I)), 1)
+        q_score = self.attn_score(attn_input)
+        # transfer to (B*I, Q, 1)
+        q_score = q_score.view(bsz, num_queries, -1)
+
+        # broadcasting [(B*I)*Q*1] * [(B*I)*1*E_lstm] -> [(B*I)*Q*E_lstm]
+
+        #  [(B*I)*Q*1] * [(B*I)*Q*E_lstm] -> [(B*I)*Q*E_lstm]
+        self_attn_rep = torch.multiply(q_score, v)
+
+        # [(B*I) * Q * E_lstm] -> [(B*I) * 1 * E_lstm]
+        summed_ha_rep = torch.sum(self_attn_rep, dim=-2)
+
+        return summed_ha_rep
+
+class LSTMHA(nn.Module):
+    def __init__(self, lstm_input_size=29, lstm_h_dim=32,
+                 lstm_num_layers=1,
+                 attn_w_dim=64, dropout=0.2):
+        """
+        接收窗口大小为K的固定长度序列作为输入
+        """
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=lstm_input_size,
+                            hidden_size=lstm_h_dim,
+                            num_layers=lstm_num_layers,
+                            batch_first=True,
+                            bidirectional=False,
+                            dropout=dropout)
+        self.ha = StdAttn(input_size=lstm_h_dim,
+                          att_dim=attn_w_dim, dropout=dropout)
+
+    def forward(self, x):
+        """
+        x的形状：batch_size * stock_num * window_size_K * feature_dim (ndim=4)
+        """
+        # if is aligned batch inputs (B * I * T * E_feat), ndim=4
+        batch_size = x.shape[0]
+        num_stock = x.shape[1]
+
+        # lstm设置为batch_first则需要输入(batch, seq_len, input_size)的数据
+        # 在这里让x变为(batch_size * stock_num, window_size_K, feature_dim)的张量
+        x = x.view(batch_size * num_stock, x.shape[-2], x.shape[-1])
+
+        # 设置了batch_fisrt=True后，单向lstm输出outputs(batch, seq_len, hidden_size), (hn, cn)
+        # 这里outputs是(batch*stock_num, window_size_K, hidden_size)
+
+        outputs, _ = self.lstm(x)
+        out_rep = self.ha(outputs, outputs[:,-1,:], outputs)
+
+        out_rep = out_rep.view(batch_size, num_stock, -1)
+
+        return out_rep
+
+class BasicCANN(nn.Module):
+    def __init__(self, num_heads=4, embed_dim=32):
+        super().__init__()
+        self.attn = nn.MultiheadAttention(num_heads=num_heads,
+                                          embed_dim=embed_dim,)
+        self.score = nn.Sequential(
+            nn.Linear(embed_dim, 1),
+            nn.Sigmoid())
+
+    def forward(self, x):
+        """
+        :param x: B*I*E_c
+        :return: B*I*1
+        """
+        # B * I * E -> B * I * E
+        cross_attn_rep, attn_weights = self.attn(x, x, x)
+        # B * I * E -> B * I * 1
+        scores = self.score(cross_attn_rep)
+        return scores, attn_weights
+
+class PortfolioGenerator(nn.Module):
+    # 固定规则，无需学习
+    def __init__(self, G=10):
+        super().__init__()
+        self.G = G
+
+    def forward(self, x):
+        """
+        :param x: B*I
+        :return:portfolio: B*I
+        """
+        # remove redundant last dim of size 1
+        x = x.squeeze(-1)
+        if x.shape[-1] <= 2 * self.G:
+            raise ValueError(f"len of configurable stocks:{2 * self.G} "
+                             f"> available stocks:{x.shape[-1]}")
+
+        # bsz * num_asset
+        # b_c = torch.zeros_like(x, requires_grad=True)
+
+        # batch-wise ranking & binarize
+        sorted_x, sorted_indices = torch.sort(x, dim=-1, descending=True)
+
+        winner_assets_x = sorted_x[:, :self.G]
+        # winner_assets_indices = sorted_indices[:, :self.G]
+        loser_assets_x = sorted_x[:, -self.G:]
+        # loser_assets_indices = sorted_indices[:, :self.G]
+
+        # --- Todo: temp: the following codes suit only bsz = 1 -----#
+        winner_proportion = F.softmax(winner_assets_x, dim=-1)
+        loser_proportion = F.softmax(1 - loser_assets_x, dim=-1)
+
+        zeros_assets = torch.zeros_like(sorted_x[:, self.G:-self.G],
+                                        requires_grad=True).type(x.dtype)
+        # sorted_winner_assets_x = winner_assets_x.sort_values(by=sorted_indices)
+        # sorted_loser_assets_x = loser_assets_x.sort_values(by=sorted_indices)
+
+        b_c = torch.cat([winner_proportion, zeros_assets,
+                         loser_proportion], dim=1)
+
+        # convert back to orig order
+        # Todo: to fix : inplace variables -> prevent differentiating
+        # b_c[:, winner_assets_indices] = winner_proportion
+        # b_c[:, loser_assets_indices] = loser_proportion
+
+        # zeros_like: 生成和括号内变量维度一致的全是0的内容
+        reordered_b_c = torch.zeros_like(b_c, requires_grad=True).type(x.dtype)
+        # out-of-place copy,  but only support one dim index
+        torch.scatter(input=reordered_b_c,
+                      index=sorted_indices,
+                      src=b_c,
+                      dim=1)
+
+        return reordered_b_c, sorted_indices
+
+class PGNetwork(nn.Module):
+    def __init__(self):
+        super(PGNetwork, self).__init__()
+        self.lstm_ha = LSTMHA()
+        self.cann = BasicCANN()
+        self.portfolio_gen = PortfolioGenerator()
+
+    def forward(self, x):
+        if x.dim() == 3:
+            x_b = x.unsqueeze(0)  # TODO: 第一个维度给batch
+        else:
+            x_b = x
+
+        # x_b: (batch_size, num_stock, window_size_K, feature_dim)
+        ha_rep = self.lstm_ha(x_b)
+        # ha_rep: (batch_size, num_stock, E_c)
+        cann_score, attn_w = self.cann(ha_rep)
+        # cann_score: (batch_size, num_stock, E_c), attn_w: (batch_size, num_stock, 1)
+        portfolios, sorted_indices = self.portfolio_gen(cann_score)
+        return portfolios, sorted_indices
+
+    # def initialize_weights(self):
+    #     for m in self.modules():
+    #         nn.init.normal_(m.weight.data, 0, 0.1)
+    #         nn.init.constant_(m.bias.data, 0.01)
+    #         # m.bias.data.zero_()
+
+class PG(object):
+    # dqn Agent
+    def __init__(self, env):  # 初始化
+        # 状态空间和动作空间的维度
+        # self.state_dim = env.observation_space.shape[0]
+        # self.action_dim = env.action_space.n
+
+        # init N Monte Carlo transitions in one game
+        self.ep_obs, self.ep_as, self.ep_rs = [], [], []
+
+        # init network parameters
+        self.network = PGNetwork().to(device)
+        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=LR)
+
+        # init some parameters
+        self.time_step = 0
+
+    def choose_action(self, observation):
+        # TODO: 解决输入state维度问题
+        observation = torch.FloatTensor(observation).to(device)
+        network_output, sorted_indices = self.network.forward(observation)
+        # network_output是portfolio向量(b_c)，正值代表买入，负值代表卖出
+
+        # with torch.no_grad():
+        #     prob_weights = F.softmax(network_output, dim=0).cuda().data.cpu().numpy()
+        # # prob_weights = F.softmax(network_output, dim=0).detach().numpy()
+        # action = np.random.choice(range(prob_weights.shape[0]),
+        #                           p=prob_weights)  # select action w.r.t the actions prob
+
+        action_prob = network_output.detach().numpy()[0]
+        # action_prob的形式: [0.4, 0.1, 0,..., -0.2, -0.3]
+        # action_prob=np.array([0.4,0.1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-0.2,-0.3])
+        action_shuffle = [np.random.binomial(1,i)
+                          if i>=0 else -np.random.binomial(1,-i)
+                          for i in action_prob
+                          ] # 按概率决定是否交易
+        # action_prob的形式: [1, 0, 0,..., -1, 0]（随机向量）
+
+        action = [0]*len(action_shuffle)
+        sorted_indices = sorted_indices.detach().numpy()[0]
+
+        for i in range(len(action_shuffle)):
+            action[sorted_indices[i]] = action_shuffle[i]
+
+
+        # 这里返回的action是一个概率向量，代表每只股票发生交易的概率，action对应位置代表对应股票
+        # TODO: 假设前面的（多头）为正，后面的（空头）为负，但向量绝对值之和归一
+        return action
+
+    # 将状态，动作，奖励这一个transition保存到三个列表中
+    def store_transition(self, s, a, r):
+        self.ep_obs.append(s)
+        self.ep_as.append(a)
+        self.ep_rs.append(r)
+
+    def learn(self):
+        self.time_step += 1
+
+        # Step 1: 计算每一步的状态价值
+        discounted_ep_rs = np.zeros_like(self.ep_rs)
+        running_add = 0
+        # 注意这里是从后往前算的，所以式子还不太一样。算出每一步的状态价值
+        # 前面的价值的计算可以利用后面的价值作为中间结果，简化计算；从前往后也可以
+        for t in reversed(range(0, len(self.ep_rs))):
+            running_add = running_add * GAMMA + self.ep_rs[t]
+            discounted_ep_rs[t] = running_add
+
+        discounted_ep_rs = np.array(discounted_ep_rs,dtype=np.float64)
+        discounted_ep_rs -= np.mean(discounted_ep_rs)  # 减均值
+        discounted_ep_rs /= np.std(discounted_ep_rs)  # 除以标准差
+        discounted_ep_rs = torch.FloatTensor(discounted_ep_rs).to(device)
+
+        # Step 2: 前向传播
+        softmax_input, _ = self.network.forward(torch.FloatTensor(self.ep_obs).to(device))
+        # all_act_prob = F.softmax(softmax_input, dim=0).detach().numpy()
+
+        neg_log_prob = F.cross_entropy(input=softmax_input, target=(torch.LongTensor(self.ep_as)).float().to(device),
+                                       reduction='none')
+
+        # Step 3: 反向传播
+        loss = torch.mean(neg_log_prob * discounted_ep_rs)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # 每次学习完后清空数组
+        self.ep_obs, self.ep_as, self.ep_rs = [], [], []
+
+# ---------------------------------------------------------
+# Hyper Parameters
+EPISODE = 3000  # Episode limitation
+STEP = 1000  # Step limitation in an episode
+TEST = 5  # The number of experiment test every 100 episode
+
+
+def main():
+    env = ALPHA_ENV()
+    agent = PG(env)
+
+    for episode in range(EPISODE):
+        state = env.reset() # 返回的state维数: (num_stock, window_size_k, feature_dim)
+        # TODO: 解决batch问题，当前只适用于batch_size=1
+        for step in range(STEP):
+            action = agent.choose_action(state)  # softmax概率选择action
+            next_state, reward, done, _ = env.step(action)
+            agent.store_transition(state, action, reward)  # 新函数 存取这个transition
+            state = next_state
+            if done:
+                # print("stick for ",step, " steps")
+                print("episode:", episode, "episode reward", np.mean(agent.ep_rs))
+                agent.learn()  # 更新策略网络
+                break
+
+        # Test every 100 episodes
+        if episode % 100 == 0:
+            total_reward = 0
+            for i in range(TEST):
+                state = env.reset()
+                for j in range(STEP):
+                    action = agent.choose_action(state)  # direct action for test
+                    state, reward, done, _ = env.step(action)
+                    total_reward += reward
+                    if done:
+                        break
+            ave_reward = total_reward / TEST
+            print('Evaluation | episode: ', episode, ' | Evaluation Average Reward:', ave_reward)
+
+
+if __name__ == '__main__':
+    time_start = time.time()
+    main()
+    time_end = time.time()
+    print('The total time is ', time_end - time_start)
